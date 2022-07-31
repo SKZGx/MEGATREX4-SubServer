106357ed040e494ad8f1cc55b75a0fb8e0627892
Eugene <skyzgamer@outlook.com>
2022-07-31 14:58:41 +0300

    upt form

---
+++ form.js
@@ -0,0 +1,38 @@
+async function sendContact(ev) {
+    ev.preventDefault();
+
+    const senderMinecaft = document
+      .getElementById('minecaftInput').value;
+    const senderTwitch = document
+      .getElementById('twitchInput').value;
+    const Senderwho = document
+        .getElementById('InputWho').value;
+
+    const webhookBody = {
+      embeds: [{
+        title: 'Заявки на серер',
+        fields: [
+          { name: 'Minecaft', value: senderMinecaft },
+          { name: 'Twitch', value: senderTwitch },
+          { name: 'чей', value: Senderwho}
+        ]
+      }],
+    };
+
+    const webhookUrl = 'https://discord.com/api/webhooks/1003257933523583077/Xjp0t0EierXJ0YEq9o1D3dj22iX1X8-w-Nc-d09ta8fNMeDZNR1EbJn-5QAK8rcslh5G';
+
+    const response = await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(webhookBody),
+    });
+
+    if (response.ok) {
+      alert('Ваша заявка отправлена, ожидайте ответа!');
+    } else {
+      alert('There was an error! Try again later!');
+    }
+
+  }
